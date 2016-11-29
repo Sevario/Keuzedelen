7506401d64c09a28f5f22b217d57f4dae568e6de
Sevario <Flipmil@hotmail.com>
2016-11-29 11:31:38 +0100

    Login fixed

--- session.php
+++ session.php
@@ -1,13 +1,13 @@
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "");
+$connection = mysql_connect("92.222.169.67", "root", "garage");
 // Selecting Database
-$db = mysql_select_db("company", $connection);
+$db = mysql_select_db("Keuzedelen", $connection);
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
+$ses_sql=mysql_query("select username from User where username='$user_check'", $connection);
 $row = mysql_fetch_assoc($ses_sql);
 $login_session =$row['username'];
 if(!isset($login_session)){
