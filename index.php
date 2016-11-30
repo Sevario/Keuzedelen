6e4d570a9a0a415a432baf2b218a79636e33abc7
Sevario <Flipmil@hotmail.com>
2016-11-30 11:08:06 +0100

    CSSTest

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
-<form role="form" class="login" action="login.php" autocomplete="off" method="POST">
-    <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-user"></i></span>
-        <input type="text" class="login_user" name="email" placeholder="Email">
-    </div>
-    <span class="help-block"></span>
 
-    <div class="input-group">
-        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-        <input  type="password" class="login_password" name="password" placeholder="Password">
-    </div>
-    <span class="help-block"></span>
+<html>
+    <head>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    </head>
+    
+    <body>
+        <form role="form" class="login" action="login.php" autocomplete="off" method="POST">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="login_user" name="email" placeholder="Email">
+            </div>
+            <span class="help-block"></span>
 
-    <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Login</button>
-</form>
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input  type="password" class="login_password" name="password" placeholder="Password">
+            </div>
+            <span class="help-block"></span>
+
+            <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Login</button>
+        </form>
+  </body>
+ </html>
