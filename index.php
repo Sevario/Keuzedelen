114308401201d7f3bc36fa9417c2e4328ba9a60a
Sevario <Flipmil@hotmail.com>
2016-11-30 10:25:13 +0100

    Update

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<form role="form" class="omb_loginForm" action="login.php" autocomplete="off" method="POST">
+<form role="form" class="login" action="login.php" autocomplete="off" method="POST">
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-user"></i></span>
-        <input type="text" class="form-control" name="email" placeholder="Email">
+        <input type="text" class="login_user" name="email" placeholder="Email">
     </div>
     <span class="help-block"></span>
 
     <div class="input-group">
         <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-        <input  type="password" class="form-control" name="password" placeholder="Password">
+        <input  type="password" class="login_password" name="password" placeholder="Password">
     </div>
     <span class="help-block"></span>
 
