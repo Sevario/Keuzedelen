b7425e908ac56d452bbc418c4266d78c3ee5bcf8
Sevario <Flipmil@hotmail.com>
2016-12-08 13:32:16 +0100

    dad
    Lalalae

--- dashboard.php
+++ dashboard.php
@@ -27,6 +27,12 @@
             <li><a>Account</a></li>
             <li><a>Beheer(Admin only)</a></li>
             <li><a href="logout.php">Log Out</a></li>
+            
+            <div id="myProgress">
+                <div id="myBar">
+                    <div id="label">1/3</div>
+                </div>
+            </div>
         </ul>
 
     </div>
