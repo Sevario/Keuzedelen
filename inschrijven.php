14d8a934c16d56911f19eb30f312159867e4976a
peterderoos <peterderoos@live.nl>
2016-12-22 14:09:39 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	inschrijven.php

--- inschrijven.php
+++ inschrijven.php
@@ -35,28 +35,19 @@ if (isset( $_GET["kd"]) && !empty($_GET["kd"]))
                                     border-radius: 2px 2px 2px 2px;
                                     margin-left: 35%;
                                     margin-right: 40%;
-<<<<<<< HEAD
                                     margin-top: 10%;'><h5 style='color: white;'>Het is niet mogelijk om je twee keer voor hetzelfde keuzedeel op te geven.
                     Je wordt teruggezonden naar de hoofdpagina.</h5></div>"; header("refresh:4;url=dashboard.php");
             }
-    } 
-Else {
-echo "An unexpected error has occurred.";
-}
-=======
-                                    margin-top: 10%;'>
-                    <h5 style='color: white;'>Het is niet mogelijk om je twee keer voor hetzelfde keuzedeel op te geven. Je wordt teruggezonden naar de hoofdpagina.</h5>
-                    </div>";
-                header("refresh:2;url=dashboard.php");
-        }
-        else {
+            else {
             $insert1 = $conn->prepare("INSERT INTO Keuzedeel_Student (`Keuzedeel_ID`, `Student_ID`, `Ingeschreven`) VALUES ($IDkeuze, $student_info[ID], 'Y');");
             $insert1->execute();
             echo "<h1>Je bent succesvol ingeschreven voor het Keuzedeel: " . $IDkeuze . "</h1>";
             echo "Je wordt teruggezonden naar de hoofdpagina.</h5></div>"; //header("refresh:2;url=dashboard.php");
 
         }
-    }
->>>>>>> origin/master
+    } 
+Else {
+echo "An unexpected error has occurred.";
+}
 ?>
 </html>
